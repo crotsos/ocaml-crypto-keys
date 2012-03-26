ba886938153d673b3f27abf58d776b1d33b1bad8
cr409 <cr409@cl.cam.ac.uk>
2012-03-26 21:13:54 +0100

    dnssec to ssh tool now allows to define ip nd port for the dns server

--- lib/dnssec_to_ssh_test.ml
+++ lib/dnssec_to_ssh_test.ml
@@ -4,11 +4,13 @@ open Lwt
 
 let fetch_data () = 
   try 
-    let domain_name = Sys.argv.(1) in 
-      lwt tmp = ssh_pub_key_of_domain domain_name in 
+    let server_ip = Sys.argv.(1) in 
+    let server_port = Sys.argv.(2) in 
+    let domain_name = Sys.argv.(3) in 
+      lwt tmp = ssh_pub_key_of_domain  ~server:server_ip ~port:(int_of_string server_port) domain_name in 
       match tmp with
         | Some(keys) ->
-          lwt _ = ssh_fingerprint_of_domain domain_name in 
+          lwt _ = ssh_fingerprint_of_domain ~server:server_ip ~port:(int_of_string server_port) domain_name in 
            Printf.printf "keys for domain %s: %s\n%!" domain_name (List.hd keys);
            return ()
         | None ->
